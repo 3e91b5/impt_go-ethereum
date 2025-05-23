5c4968f317c19f60d87211c964ff1a43258c4bbe
3e91b5 <jaehunkim@snu.ac.kr>
2025-05-23 17:03:54 +0900

    add debug logs

--- core/state_processor.go
+++ core/state_processor.go
@@ -24,6 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
@@ -95,6 +96,11 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 	// Create a new environment which holds all relevant information
 	// about the transaction and calling mechanisms.
 	vmenv := vm.NewEVM(context, statedb, config, cfg)
+
+	//jhkim
+	// debug log for applytransaction
+	log.Debug("ApplyTransaction", "to", msg.To(), "data", msg.Data())
+
 	// Apply the transaction to the current state (included in the env)
 	_, gas, failed, err := ApplyMessage(vmenv, msg, gp)
 	if err != nil {
